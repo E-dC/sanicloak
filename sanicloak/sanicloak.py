93268adf0b25b8c8bf77bd7244500627da607d20
EdC <22207434+E-dC@users.noreply.github.com>
2022-04-21 16:24:45 +0200

    Add basic docs for methods, fix sanic.Response to sanic.HTTPResponse in method typings.

--- sanicloak/sanicloak.py
+++ sanicloak/sanicloak.py
@@ -41,7 +41,8 @@ class KeycloakAuthenticator(object):
         self.keycloak_login_url = self.keycloak_client.auth_url(self.redirect_url)
         self.app = app
 
-    async def get_token_from_code(self, request: sanic.Request):
+    async def get_token_from_code(self, request: sanic.Request) -> Dict[str, Any]:
+        """Exchange a Keycloak authorization code for access and refresh tokens"""
 
         try:
             code = request.args['code'][0]
@@ -62,7 +63,8 @@ class KeycloakAuthenticator(object):
         return token
 
     async def clear_cookies(
-            self, response: sanic.Response, *cookies: str) -> sanic.Sanic:
+            self, response: sanic.HTTPResponse, *cookies: str) -> sanic.Sanic:
+        """Remove cookies from response"""
 
         for cookie in cookies:
             try:
@@ -73,11 +75,12 @@ class KeycloakAuthenticator(object):
 
     async def set_cookies(
             self,
-            response: sanic.Response,
+            response: sanic.HTTPResponse,
             httponly: bool = True,
             secure:bool = False,
             max_age: int = 3600,
-            **cookies: Union[bool, int]) -> sanic.Response:
+            **cookies: Union[bool, int]) -> sanic.HTTPResponse:
+        """Set cookies in response"""
 
         for cname, cvalue in cookies.items():
             response.cookies[cname] = cvalue
@@ -90,8 +93,11 @@ class KeycloakAuthenticator(object):
 
     async def add_requested_url(
             self,
-            response: sanic.Response,
-            requested_url: str) -> sanic.Response:
+            response: sanic.HTTPResponse,
+            requested_url: str) -> sanic.HTTPResponse:
+        """ Add a requested_url cookie so that we can redirect towards the page
+            originally requested by the user
+        """
 
         if not response.cookies.get('requested_url'):
             x = parse_url(requested_url)
@@ -101,6 +107,7 @@ class KeycloakAuthenticator(object):
         return response
 
     async def check_requested_url(self, requested_url: str) -> bool:
+        """Ensure the requested_url value does not lead outside the app routes"""
 
         x = parse_url(requested_url)
         base = f'{x.scheme}://{x.netloc}'
@@ -113,7 +120,8 @@ class KeycloakAuthenticator(object):
 
     async def retrieve_token(
             self,
-            request: sanic.Request) -> Union[sanic.Request, sanic.Response]:
+            request: sanic.Request) -> Union[sanic.Request, sanic.HTTPResponse]:
+        """Ensure appropriate tokens are present before a request reach its handler"""
 
         logger.debug('> Retrieve token (request)')
 
@@ -142,7 +150,9 @@ class KeycloakAuthenticator(object):
 
     async def validate_token(
             self,
-            request: sanic.Request) -> Union[sanic.Request, sanic.Response]:
+            request: sanic.Request) -> Union[sanic.Request, sanic.HTTPResponse]:
+        """ Ensure access token is valid before a request reach its handler,
+            refresh if necessary"""
 
         logger.debug('> Validate token (request)')
 
@@ -177,7 +187,9 @@ class KeycloakAuthenticator(object):
 
     async def redirect_to_origin(
             self,
-            request: sanic.Request) -> Union[sanic.Request, sanic.Response]:
+            request: sanic.Request) -> Union[sanic.Request, sanic.HTTPResponse]:
+        """ Redirect to the URL originally requested by the user, if it is different
+            from self.redirect_url"""
 
         logger.debug('> Ensure route used is the one originally requested (request)')
 
@@ -198,7 +210,10 @@ class KeycloakAuthenticator(object):
     async def set_keycloak_cookies(
             self,
             request: sanic.Request,
-            response: sanic.Response) -> sanic.Response:
+            response: sanic.HTTPResponse) -> sanic.HTTPResponse:
+        """ Set Keycloak cookies in response so that user does not need to login
+            again after next request
+        """
 
         logger.debug('> Set Keycloak cookies (response)')
 
@@ -239,6 +254,9 @@ class KeycloakAuthenticator(object):
             self,
             request: sanic.Request,
             roles: Union[List, Set, Tuple]) -> sanic.Request:
+        """ Check if the user has an authorized role, by checking
+            the user's identity token
+        """
 
         try:
             user_roles = set(request.ctx.identity_token['roles'])
@@ -251,6 +269,7 @@ class KeycloakAuthenticator(object):
         return request
 
     def protected(self, maybe_func=None, *, roles=None):
+        """Decorator to use so that app routes are protected"""
         def decorator(handler):
             @wraps(handler)
             async def decorated_function(request, *args, **kwargs):
